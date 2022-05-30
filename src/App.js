d040234d0a1e1a32d8a33e664075754a6db867c4
Анна <anna@MacBook-Air.local>
2022-05-30 10:02:50 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, { useEffect } from "react"
+import Navigation from "./components/Navigation";
+import Dead from "./components/Dead";
+import Form from "./components/Form";
+import List from "./components/List";
+import Space from "./components/Space";
+import Tasks from "./components/Tasks";
+import Wishlist from "./components/Wishlist";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+export default function App() {
+  const [plants, setPlants] = React.useState([])
+
+
+  useEffect(
+    () => {fetch('http://127.0.0.1:8000/getlist/')
+    .then (response => response.json())
+    .then (response => setPlants(response))
+  }, 
+    []
   );
-}
 
-export default App;
+  return (
+    <Router>
+    <div class="container-fluid gx-0">
+    <div class="row">
+    <div class="col-3">
+    <Navigation />
+    </div>
+    <div class="col-8">
+    <Routes>
+      <Route path="/tasks" element={<Tasks />} />
+      <Route path="/list" element={<List 
+      plants={plants}/>} />
+      <Route path="/wishlist" element={<Wishlist 
+      plants={plants}/>} />
+      <Route path="/dead" element={<Dead />} />
+      <Route path="/form" element={<Form />} />
+      <Route path="/space" element={<Space />} />
+    </Routes>
+    </div>
+    </div>
+    </div>
+  </Router>
+  ) 
+}
